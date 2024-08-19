8ccb4aad452c684ef5e49d04fbad48f546c3ca91
Uros Ristanovic <urosristanovic@gmail.com>
2024-08-19 16:07:59 +0200

    feat: implement session (cookies), add sign out option

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+// Because of tsconfig, we can not just import { CookieSession } from cookie-session, so we have to do it this way
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      keys: ['uroskey'],
+    }),
+  );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
