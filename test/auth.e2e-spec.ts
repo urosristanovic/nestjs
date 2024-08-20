67de7615fd0ba51bd7a1b6743f79ecf2c520bd7d
Uros Ristanovic <urosristanovic@gmail.com>
2024-08-20 15:40:42 +0200

    feat: add multiple databases for different envs, add new tests, and config application

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System (e2e)', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'uros_e2e_03@gmail.com';
+    const email = 'uros_e2e_01@gmail.com';
 
     return request(app.getHttpServer())
       .post('/auth/signup')
@@ -30,5 +30,23 @@ describe('Authentication System (e2e)', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'uros_e2e@gmail.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '12345' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
